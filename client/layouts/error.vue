93c26ced7dae0f3115d4c721d8b4d5ace4515b24
Andy Joset <andyjoset1997@hotmail.com>
2021-06-30 22:55:22 -0400

    Remove duplicated components

--- client/layouts/error.vue
+++ client/layouts/error.vue
@@ -1,44 +1,21 @@
 <template>
-    <v-app dark>
-        <app-navbar />
-        <v-main>
-            <v-row align="center" justify="center">
-                <v-col cols="12" sm="8" md="6">
-                    <div class="px-4 pt-4 pb-3">
-                        <div class="mt-8 text-h1">
-                            Error {{ error.statusCode }}
-                        </div>
-                        <div class="text-h2">
-                            {{ error.message }}
-                        </div>
-                        <v-btn nuxt text :to="{ name: 'home' }">
-                            <v-icon class="mr-1">mdi-home</v-icon> Home page
-                        </v-btn>
-                    </div>
-                </v-col>
-            </v-row>
-        </v-main>
-        <app-footer />
-        <snackbar />
-
-        <overlay-loader />
-    </v-app>
+    <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="6">
+            <div class="mt-8 text-h1">
+                Error {{ error.statusCode }}
+            </div>
+            <div class="text-h4">
+                {{ error.message }}
+            </div>
+            <v-btn nuxt color="primary" :to="{ name: 'home' }" class="mt-2">
+                <v-icon class="mr-1">mdi-home</v-icon> Home page
+            </v-btn>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
-    import AppFooter from '~/components/AppFooter'
-    import AppNavbar from '~/components/AppNavbar'
-    import Snackbar from '~/components/Snackbar'
-    import OverlayLoader from '~/components/OverlayLoader'
-
     export default {
-        components: {
-            AppNavbar,
-            AppFooter,
-            Snackbar,
-            OverlayLoader,
-        },
-
         layout: 'empty',
 
         props: {
@@ -56,9 +33,3 @@
         }),
     }
 </script>
-
-<style scoped>
-    h1 {
-        font-size: 20px;
-    }
-</style>
