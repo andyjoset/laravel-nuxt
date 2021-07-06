0ef7019a0f24362d00710da8ad1e4dbec43ab148
Andy Joset <andyjoset1997@hotmail.com>
2021-07-06 14:52:21 -0400

    Add seeder for roles and permissions

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Role::create(['name' => 'Super Admin']);
+
+        Permission::create(['name' => 'users.index']);
+        Permission::create(['name' => 'users.store']);
+        Permission::create(['name' => 'users.update']);
+        Permission::create(['name' => 'users.destroy']);
+        Permission::create(['name' => 'users.toggle']);
+    }
+}
