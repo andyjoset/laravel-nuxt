8eb973b28e46fa3536703df0ffd27a27e662dae8
Andy Joset <andyjoset1997@hotmail.com>
2021-07-06 22:14:53 -0400

    Create permissions to manage roles

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -20,11 +20,18 @@ class RolesAndPermissionsSeeder extends Seeder
 
         Role::create(['name' => 'Super Admin']);
 
+        // Users
         Permission::create(['name' => 'users.index']);
         Permission::create(['name' => 'users.store']);
         Permission::create(['name' => 'users.update']);
         Permission::create(['name' => 'users.delete']);
         Permission::create(['name' => 'users.toggle']);
         Permission::create(['name' => 'users.assign-role']);
+
+        // Roles
+        Permission::create(['name' => 'roles.index']);
+        Permission::create(['name' => 'roles.store']);
+        Permission::create(['name' => 'roles.update']);
+        Permission::create(['name' => 'roles.delete']);
     }
 }
